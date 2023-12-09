f960d18a39356865d9f362de42b291ea9bb9ab01
iradbouzidi <b.irad1994@gmail.com>
2023-12-09 16:38:47 +0100

    test

--- grafbase/grafbase.config.ts
+++ grafbase/grafbase.config.ts
@@ -1,48 +1,21 @@
-import { g, config, auth } from "@grafbase/sdk";
+// g is a schema generator, config the final object to return
+import { g, config } from "@grafbase/sdk";
 
-// @ts-ignore
-const User = g
-  .model("User", {
-    name: g.string().length({ min: 2, max: 100 }),
-    email: g.string().unique(),
-    avatarUrl: g.url(),
-    description: g.string().length({ min: 2, max: 1000 }).optional(),
-    githubUrl: g.url().optional(),
-    linkedinUrl: g.url().optional(),
-    projects: g
-      .relation(() => Project)
-      .list()
-      .optional(),
-  })
-  .auth((rules) => {
-    rules.public().read();
-  });
-
-// @ts-ignore
-const Project = g
-  .model("Project", {
-    title: g.string().length({ min: 3 }),
-    description: g.string(),
-    image: g.url(),
-    liveSiteUrl: g.url(),
-    githubUrl: g.url(),
-    category: g.string().search(),
-    createdBy: g.relation(() => User),
-  })
-  .auth((rules) => {
-    rules.public().read();
-    rules.private().create().delete().update();
-  });
-
-const jwt = auth.JWT({
-  issuer: "grafbase",
-  secret: g.env("NEXTAUTH_SECRET"),
+// types are generated with the `type` method,
+// followed by the name and fields.
+const profile = g.type("Profile", {
+  address: g.string(),
 });
 
+// models can be generated with the `model` method
+const user = g.model("User", {
+  name: g.string(),
+  age: g.int().optional(),
+  profile: g.ref(profile).optional(),
+  parent: g.relation(() => user).optional(),
+});
+
+// finally we export the default config
 export default config({
   schema: g,
-  auth: {
-    providers: [jwt],
-    rules: (rules) => rules.private(),
-  },
 });
